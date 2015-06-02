ac02b850b9a8b719525b1e39c51d0c1568a41ed4
matthewko <zaeni.redmage@gmail.com>
2015-06-02 23:44:42 +0100

    feat(listview): Added listview

--- app/src/main/java/com/codingprojects/mko/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/codingprojects/mko/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -18,6 +22,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> weekForecast = new ArrayList<>();
+        weekForecast.add("Today Sunny 88/63");
+        weekForecast.add("Today Sunny 48/23");
+        weekForecast.add("Today Sunny 78/63");
+        weekForecast.add("Today Sunny 78/53");
+        weekForecast.add("Today Sunny 68/33");
+        weekForecast.add("Today Sunny 88/43");
+        weekForecast.add("Today Sunny 88/33");
+        //getActivity returns this fragment's parent activity hence the context
+        //R.layout.list_item_forecast is the ID of the list item layout
+        //R.id.list_item_forecast_textview is the ID of the textview to populate.
+        //forecast data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+        return rootView;
     }
 }
